7976061125cd264caa14964556f8b0f4ae6c1b65
Tomy Huisa <huisatomy2007@gmail.com>
2024-09-15 11:53:31 -0300

    Edite el archivo principal.py para la verificacion de la clase apuesta

--- principal.py
+++ principal.py
@@ -1,9 +1,20 @@
 from perinola import Perinola
+from apuesta import Apuesta
 
 p = Perinola()
 print(p)
 print(p.cara_visible)
-resultado = p.tirar()
+resultado= p.tirar()
 print(resultado)
 print(p)
 print(p.cara_visible)
+print("Perinola Arriba| Apuesta Abajo|")
+a = Apuesta()
+print(a)
+a.ponerFicha(4)
+print(a)
+a.tomarFicha(8)
+print(a)
+a.tomarFicha()
+print(a)
+
